0df1782a51bf65d660bbfb4bac2a3ccfdea8d18e
Isak Silva <isakfreires98@gmail.com>
2025-01-21 21:56:20 -0300

    feat: implement checkWinner function to determine game outcome

--- script.js
+++ script.js
@@ -27,11 +27,39 @@ function Gameboard() {
   return { getBoard, printBoard, markCell };
 }
 
-const gameBoard = Gameboard();
-const board = gameBoard.getBoard();
-gameBoard.markCell(0, 0, "X");
-gameBoard.markCell(0, 1, "X");
-gameBoard.markCell(0, 2, "X");
+function GameController() {
+  let currentPlayer = "X";
+  let winner = null;
+  const board = Gameboard.getBoard();
 
-console.log(gameBoard.printBoard());
-console.log(board);
+  const switchPlayer() => {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+  };
+
+  const checkWinner = (board) => {
+    const winConditions = [
+      
+      [board[0][0], board[0][1], board[0][2]],
+      [board[1][0], board[1][1], board[1][2]],
+      [board[2][0], board[2][1], board[2][2]],
+      //columns
+      [board[0][0], board[1][0], board[2][0]],
+      [board[0][1], board[1][1], board[2][1]],
+      [board[0][2], board[1][2], board[2][2]],
+      // Diagonals
+      [board[0][0], board[1][1], board[2][2]],
+      [board[0][2], board[1][1], board[2][0]],
+    ];
+
+    // biome-ignore lint/style/useConst: <explanation>
+    for(let condition of winConditions){
+      if(
+        condition[0] !== null && 
+        condition[0] === condition[1] && 
+        condition[1] === condition[2]
+      ){
+        return condition[0]}
+    }
+  }
+  return null;
+}
